765299ae88810beda4f940d79777964a6b25cbf1
Matheus <matheus.venturini@acad.ufsm.br>
2024-08-23 08:58:05 -0300

    Correções gerais, adicionadas as docstrings de calcular_tensao e calcular_corrente

--- src/simulador_de_circuito_rc/__about__.py
+++ src/simulador_de_circuito_rc/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024-present Matheus <matheus.venturini@acad.ufsm.br>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+__version__ = "1.0.0"
--- src/simulador_de_circuito_rc/simulador.py
+++ src/simulador_de_circuito_rc/simulador.py
@@ -1,17 +1,34 @@
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtCore import Qt, QTimer, QPointF
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFormLayout, QErrorMessage, QFileDialog
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFormLayout, QErrorMessage, QFileDialog
 import sys
 import numpy as np
 import csv
 from datetime import datetime
 
-def calcula_tensao(V0, R, C, t):
+def calcular_tensao(V0, R, C, t):
+    """
+    Calcula o valor de tensão do circuito RC em um instante t dados V0, R e C.
+
+    :param V0: Tensão inicial em Volts.
+    :param R: Valor do resistor em Ohms.
+    :param C: Valor da capacitância em Farads.
+    :param t: Instante de tempo em segundos.
+    :return: Valor de tensão em Volts.
+    """
     return V0 * np.exp(-t / (R * C))
 
-def calcula_corrente(V0, R, C, t):
+def calcular_corrente(V0, R, C, t):
+    """
+    Calcula o valor de corrente do circuito RC em um instante t dados V0, R e C.
+
+    :param V0: Tensão inicial em Volts.
+    :param R: Valor do resistor em Ohms.
+    :param C: Valor da capacitância em Farads.
+    :param t: Instante de tempo em segundos.
+    :return: Valor de corrente em Amperes.
+    """
     return (V0 / R) * np.exp(-t / (R * C))
 
 class Simulador(QMainWindow):
@@ -33,10 +50,10 @@ class Simulador(QMainWindow):
         layout_formulario.addRow(QLabel("Tensão Inicial (V0) em Volts:"), self.entrada_V0)
 
         self.entrada_t_final = QLineEdit(self)
-        layout_formulario.addRow(QLabel("Tempo final em segundoss:"), self.entrada_t_final)
+        layout_formulario.addRow(QLabel("Tempo final em segundos:"), self.entrada_t_final)
 
         self.entrada_step_tempo = QLineEdit(self)
-        layout_formulario.addRow(QLabel("Passo de Tempo em segundos:"), self.entrada_step_tempo)
+        layout_formulario.addRow(QLabel("Passo de tempo em segundos:"), self.entrada_step_tempo)
 
         # Adiciona o layout de formulário ao layout principal
         layout.addLayout(layout_formulario)
@@ -45,8 +62,8 @@ class Simulador(QMainWindow):
         botao_gerar_grafico.clicked.connect(self.gerar_grafico)
         layout.addWidget(botao_gerar_grafico)
 
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
+        self.figura_gerada = plt.figure()
+        self.canvas = FigureCanvas(self.figura_gerada)
         layout.addWidget(self.canvas)
 
         self.botao_salvar = QPushButton("Salvar resultados")
@@ -59,66 +76,66 @@ class Simulador(QMainWindow):
         self.setCentralWidget(container)
 
     def gerar_grafico(self):
-        # Obtenha os valores de entrada do usuário
         try:
             R = float(self.entrada_R.text())
             C = float(self.entrada_C.text())
             V0 = float(self.entrada_V0.text())
             t_final = float(self.entrada_t_final.text())
-            passo_tempo = float(self.entrada_step_tempo.text())
+            step_tempo = float(self.entrada_step_tempo.text())
         except ValueError:
-            # Mostre uma mensagem de erro se os valores de entrada não forem válidos
             msg_erro = QErrorMessage()
             msg_erro.setWindowTitle("Erro")
-            msg_erro.showMessage("Certifique-se de que os valores numéricos inseridos são válidos")
+            msg_erro.showMessage("Certifique-se de que os valores numéricos inseridos são válidos.")
             msg_erro.exec_()
             return
 
-        # Gerar o eixo do tempo
-        self.linha_tempo = np.arange(0, t_final, passo_tempo)
+        self.linha_tempo = np.arange(0, t_final, step_tempo)
 
-        # Calcular tensão e corrente
-        self.V_transiente = calcula_tensao(V0, R, C, self.linha_tempo)
-        self.I_transiente = calcula_corrente(V0, R, C, self.linha_tempo)
+        self.V_transiente = calcular_tensao(V0, R, C, self.linha_tempo)
+        self.I_transiente = calcular_corrente(V0, R, C, self.linha_tempo)
 
-        # Limpar o gráfico anterior
-        self.figure.clear()
+        self.figura_gerada.clear() # Limpa o gráfico que tenha sido gerado anteriormente
 
-        # Plotar novos dados
-        ax = self.figure.add_subplot(111)
-        ax.plot(self.linha_tempo, self.V_transiente, label = "Tensão (V)", c = 'b')
-        ax.plot(self.linha_tempo, self.I_transiente, label = "Corrente (I)", c = 'r')
-        ax.set_xlabel("Tempo (s)")
-        ax.set_ylabel("Amplitude")
-        ax.set_title("Resposta Transitória do Circuito RC")
-        ax.legend()
+        graf = self.figura_gerada.add_subplot(111)
+        graf.plot(self.linha_tempo, self.V_transiente, label = "Tensão (V)", c = 'b')
+        graf.plot(self.linha_tempo, self.I_transiente, label = "Corrente (A)", c = 'r')
+        graf.set_xlabel("Tempo (s)")
+        graf.set_ylabel("Amplitude")
+        graf.set_title("Resposta transitória do circuito RC")
+        graf.grid()
+        graf.legend()
 
-        # Atualizar o canvas
         self.canvas.draw()
-        self.botao_salvar.setEnabled(True)
+        self.botao_salvar.setEnabled(True) # Habilita o botão de salvar os resultados
 
     def salvar_resultados(self):
-        # Abrir diálogo para selecionar onde salvar o arquivo
-        options = QFileDialog.Options()
-        nome_arquivo = 'resultados_circuito_rc_' + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + '.csv'
-        file_path, _ = QFileDialog.getSaveFileName(self, "Salvar resultados", nome_arquivo, "CSV Files (*.csv);;All Files (*)", options=options)
+        """
+        Converte a temperatura de Celsius para Fahrenheit.
 
-        if file_path:
+        :param temp_celsius: Temperatura em Celsius.
+        :return: Temperatura em Fahrenheit.
+        :raise Exception: Apenas para certificar-se de que o salvamento do arquivo tenha ocorrido com sucesso, é realizado um teste.
+        """
+        nome_arquivo = 'resultados_circuito_rc_' + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + '.csv'
+        caminho_arquivo, _ = QFileDialog.getSaveFileName(self, "Salvar resultados", nome_arquivo, "CSV Files (*.csv);;All Files (*)")
+        # Na linha acima, _ é usado como uma convenção idiomática para indicar que o valor retornado não será útil e pode ser descartado.
+        # O segundo valor retornado por getSaveFileName é o filtro utilizado (neste caso, .csv).
+
+        if caminho_arquivo:
             try:
-                # Salvar os dados em um arquivo CSV
-                with open(file_path, mode = 'w', newline = '') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(["Tempo (s)", "Tensão (V)", "Corrente (I)"])
+                # O argumento newline de open faz com que não seja inserido nenhum caracter de fim de linha no arquivo a ser escrito
+                with open(caminho_arquivo, mode = 'w', newline = '') as arquivo:
+                    writer = csv.writer(arquivo)
+                    writer.writerow(["Tempo (s)", "Tensão (V)", "Corrente (A)"])
                     for i in range(len(self.linha_tempo)):
                         writer.writerow([self.linha_tempo[i], self.V_transiente[i], self.I_transiente[i]])
 
-                # Exibir um tooltip de sucesso
-                self.entrada_R.setToolTip(f"Resultados salvos em: {file_path}")
-                self.entrada_R.setStyleSheet("border: 1px solid green;")
             except Exception as e:
-                # Exibir um tooltip de erro
-                self.entrada_R.setToolTip(f"Erro ao salvar o arquivo: {str(e)}")
-                self.entrada_R.setStyleSheet("border: 1px solid red;")
+                msg_erro = QErrorMessage()
+                msg_erro.setWindowTitle("Erro")
+                msg_erro.showMessage("Ocorreu erro ao salvar o arquivo com os resultados. O erro é este: " + str(e))
+                msg_erro.exec_()
+                return
 
 if __name__ == '__main__': # Convenção idiomática para se certificar que o programa não está sendo executado importado por algum outro arquivo
     meu_simulador = QApplication(sys.argv)
